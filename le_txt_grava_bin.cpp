152b62a8bb2934ec195f70d568a21692f40b3fc1
Nicolas-FIPP <tr19496@gmail.com>
2023-08-11 00:31:24 -0300

    up

---
+++ le_txt_grava_bin.cpp
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct nomes{
+	char nome[60];
+	char sobrenome[60];
+	Autor_lista *prox;
+};
+
+struct Autor_lista{
+	nomes * nome;
+	Autor_lista *prox;
+};
+//typedef struct Autor_lista ListaAutor;
+
+struct Livros_lista{
+	
+		Livros_lista *prox;
+		Livros_lista *ant;
+		char titulo[60];
+		int ano;
+		int pag;
+		Autor_lista **C;
+	
+};
+//typedef struct Livros_lista ListaLivros;
+
+struct editora{
+	
+	Livros_lista *C;
+	char nome[60];
+	editora *prox;
+	
+};
+//typedef struct editora Editora;
+
+struct TpRegistro 
+{
+	char autores[100],titulo_livro[100], editora[50];
+	int ano, paginas;
+};
+
+
+editora *InsereEdit(TpRegistro A,editora *C)
+{
+	editora *aux = (editora*)malloc(sizeof(editora));
+	*aux = C;
+	while(aux->prox != NULL & stricmp(aux->prox->nome,A.editora)!= 0) aux = aux->prox;
+	
+	if(aux->prox == NULL && stricmp(aux->prox->nome,A.editora)!= 0)
+	{
+		aux->prox = (editora*)malloc(sizeof(editora));
+		aux->prox->prox = NULL;
+		aux->C = (Livros_lista*)malloc(sizeof(Livros_lista));
+		aux->C->prox =	Livros_lista*)malloc(sizeof(Livros_lista));
+		aux->C->ant =	(Livros_lista*)malloc(sizeof(Livros_lista));
+		aux->C->prox =	 aux->C->ant = NULL;
+		aux->C = NULL;
+		
+		
+	}
+	
+	return C;
+}
+
+void imprime()
+{
+	FILE *arqBin = fopen("livros.dat","rb");
+    TpRegistro reg;
+    fread(&reg,sizeof(TpRegistro),1,arqBin);
+    while (!feof(arqBin))
+	{	
+    	printf("%s\n%s\n%s\t%d\t%d\n\n",reg.autores, reg.titulo_livro, reg.editora, reg.ano, reg.paginas);
+        fread(&reg,sizeof(TpRegistro),1,arqBin);
+    }  
+}
+
+void gera_arq_bin()
+{
+	FILE *arq = fopen("livros.txt","r");
+    TpRegistro reg;
+    FILE *arqBin = fopen("livros.dat","wb");
+    fscanf(arq,"%[^|]|%[^|]|%[^|]|%d|%d", &reg.autores, &reg.titulo_livro, &reg.editora, &reg.ano, &reg.paginas);
+    while(!feof(arq))
+	{
+    //	printf("%s\t%s\t%s\t%d\t%d\n",reg.autores, reg.titulo, reg.editora, reg.ano, reg.pag );
+    	fwrite(&reg,sizeof(TpRegistro),1,arqBin);
+    	fscanf(arq,"%[^|]|%[^|]|%[^|]|%d|%d", &reg.autores, &reg.titulo_livro, &reg.editora, &reg.ano, &reg.paginas);
+    };
+    fclose(arq);
+    fclose(arqBin);
+    printf("\n\nConcluido\n");
+		
+}
+
+int main(void)
+{
+	editora *CabecaEditora = (editora*)malloc(sizeof(editora));
+	*CabecaEditora = NULL;
+	
+	gera_arq_bin();
+	imprime();
+	return 0;
+}
